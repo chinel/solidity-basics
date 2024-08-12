936f8a6f1e9293733e2e64c2e0a24c0906bb9915
chinel <nelokwaz@gmail.com>
2024-08-12 13:26:08 +0100

    feat: add mapping to mapping example

--- contracts/ExampleMappings.sol
+++ contracts/ExampleMappings.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.26;
 contract ExampleMappings{
     mapping(uint => bool) public myMapping;
     mapping(address => bool) public myAddressMapping; // you can also use this example for white listing
+    mapping(uint => mapping(uint => bool)) public uintUintBoolMapping; // this can be used for balances and token contracts or allowances, votings for governance contract
 
     function setValue (uint _index) public {
         myMapping[_index] = true;
@@ -11,5 +12,9 @@ contract ExampleMappings{
 
     function setMyAddressToTrue() public {
         myAddressMapping[msg.sender] = true;
+    }
+
+    function setUintUintBoolMapping(uint _key1, uint _key2, bool _value) public {
+        uintUintBoolMapping[_key1][_key2] = _value;
     }
 }
