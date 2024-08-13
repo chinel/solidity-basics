19602d52a9e7620af5e3391f9fa0ce5fa3ae22e0
chinel <nelokwaz@gmail.com>
2024-08-13 17:35:06 +0100

    feat: add example use case of mapping

--- contracts/ExampleMapping2.sol
+++ contracts/ExampleMapping2.sol
@@ -1,6 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-contract ExampleMapping2 {
-    
+contract ExampleMappingWithdrawals {
+    mapping(address => uint) public balanceReceived;
+
+    function sendMoney () public payable  {
+        balanceReceived[msg.sender] += msg.value;
+    }
+
+   function getBalance () public view returns (uint){
+    return address(this).balance;
+   }
+
+   function withdrawAllMoney (address payable _to) public {
+       _to.transfer(balanceReceived[msg.sender]);
+       balanceReceived[msg.sender] = 0;
+   }
+
 }
