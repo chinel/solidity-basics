82e5806a26c2f7501730e075594b5cf8723eb874
chinel <nelokwaz@gmail.com>
2024-08-21 09:00:55 +0100

    feat: add mapping and struct example

--- contracts/ExampleMappingStruct.sol
+++ contracts/ExampleMappingStruct.sol
@@ -2,14 +2,34 @@
 pragma solidity 0.8.26;
 
 contract Wallet{
-    mapping(address => uint) public balance;
+
+    struct Transaction{
+        uint amount;
+        uint timestamp;
+    }
+
+    struct Balance {
+        uint totalBalance;
+        uint numDeposit;
+        mapping (uint => Transaction) deposits;
+        uint numWithdrawals;
+        mapping(uint => Transaction) withdrawals;
+    }
+
+
+    mapping(address => Balance) public balances;
 
     function depositMoney () public  payable {
-        balance[msg.sender]  += msg.value;
+        balances[msg.sender].totalBalance  += msg.value;
+        Transaction memory deposit = Transaction(msg.value, block.timestamp);
+
+        balances[msg.sender].deposits[balances[msg.sender].numDeposit] = deposit;
+        balances[msg.sender].numDeposit++;
+
     }
 
     function withdrawMoney (address payable  _to, uint _amount) public{
-         balance[msg.sender]  -= _amount;
+         balances[msg.sender].numWithdrawals  += 1;
          _to.transfer(_amount);
     }
 }
