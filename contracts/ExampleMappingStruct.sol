a4fcb1d37dac91b22825ecc8d41f5d0a8040ed9a
chinel <nelokwaz@gmail.com>
2024-08-26 21:27:56 +0100

    feat: add the withdrawal method for wallet example

--- contracts/ExampleMappingStruct.sol
+++ contracts/ExampleMappingStruct.sol
@@ -19,6 +19,10 @@ contract Wallet{
 
     mapping(address => Balance) public balances;
 
+    function getDepositNum (address _from, uint _numDeposits) public view returns(Transaction memory){
+     return  balances[_from].deposits[_numDeposits];
+    }
+
     function depositMoney () public  payable {
         balances[msg.sender].totalBalance  += msg.value;
         Transaction memory deposit = Transaction(msg.value, block.timestamp);
@@ -29,7 +33,14 @@ contract Wallet{
     }
 
     function withdrawMoney (address payable  _to, uint _amount) public{
-         balances[msg.sender].numWithdrawals  += 1;
+         balances[msg.sender].totalBalance -= _amount;
+
+         Transaction memory withdrawal = Transaction(_amount, block.timestamp);
+
+         balances[msg.sender].withdrawals[balances[msg.sender].numWithdrawals] = withdrawal;
+
+         balances[msg.sender].numWithdrawals++;
+
          _to.transfer(_amount);
     }
 }
