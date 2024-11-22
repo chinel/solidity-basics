a25a252e24ea68c322caad0c73f87d08e31f3fdd
chinel <nelokwaz@gmail.com>
2024-11-22 08:28:12 +0100

    feat: add example coffee toke using OpenZeppellin

---
+++ contracts/SampleCoffeeToken.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract CoffeToken is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    event coffeePurchased(address indexed receiver, address indexed  buyer);
+
+    constructor(address defaultAdmin, address minter) ERC20("CoffeToken", "CFE") {
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(MINTER_ROLE, minter);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function buyOneCoffee() public {
+      _burn(_msgSender(), 1); //this function is inherited from the parent contract, _msgSender() is also the same thing as msg.sender but it is inherited from the parent contract
+      emit coffeePurchased(_msgSender(), _msgSender());
+    }
+
+    function buyOneCoffeeFrom(address account) public {
+        _spendAllowance(account, _msgSender(), 1);
+        _burn(account, 1);
+        emit coffeePurchased(_msgSender(), account);
+
+    }
+}
